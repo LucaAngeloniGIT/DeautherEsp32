1312ea832c2a643eaaf0cb301f4bfa193168654f
Luca Angeloni <angeloniluca07@gmail.com>
2023-10-10 23:25:46 -0300

    V0.1

---
+++ Proyecto risinek/build-main-Desktop_x86_windows_msys_pe_32bit-Debug/qtcsettings.cmake
@@ -0,0 +1,9 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "C:/Temp/Git/Gitbash/DeautherEsp32/Proyecto risinek/build-main-Desktop_x86_windows_msys_pe_32bit-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "" CACHE "PATH" "" FORCE)
+set("CMAKE_C_COMPILER" "C:/MinGW/bin/gcc.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "C:/MinGW/bin/g++.exe" CACHE "FILEPATH" "" FORCE)
